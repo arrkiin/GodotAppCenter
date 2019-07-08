826ed97003e19cea614e248cfbf9f4a78136c559
Jens <arrkiin@gmail.com>
2019-07-08 14:31:51 +0200

    Add draft for ios

--- appcenter/ios/src/godotAppCenter.mm
+++ appcenter/ios/src/godotAppCenter.mm
@@ -1,8 +1,7 @@
 #include "godotAppCenter.h"
-#import <AppCenter/AppCenter.h>
-@import AppCenter;
-@import AppCenterAnalytics;
-@import AppCenterCrashes;
+#import "AppCenter"
+#import "AppCenterAnalytics"
+#import "AppCenterCrashes"
 
 #define CLASS_DB ClassDB
 
@@ -15,10 +14,53 @@ GodotAppCenter::~GodotAppCenter() {
     instance = NULL;
 }
 
-void GodotAppCenter::start() {
-    
+bool GodotAppCenter::isEnabled(){
+    return [MSAppCenter isEnabled];
+}
+
+void GodotAppCenter::setEnabled(boolean value){
+    [MSAppCenter setEnabled:value];
+}
+
+void GodotAppCenter::setUserId(String userId){
+    NSString *userIdStr = [[NSString alloc] initWithUTF8String:name.utf8().get_data()]
+    [MSAppCenter setUserId:userIdStr];
+}
+
+void GodotAppCenter::setLogLevel(int level){
+    [MSAppCenter setLogLevel:(NSUInteger)level];
+}
+
+void GodotAppCenter::analyticsTrackSimpleEvent(String name) {
+    NSString *eventName = [[NSString alloc] initWithUTF8String:name.utf8().get_data()];
+    NSLog(@"AppCenter.Analytics: SimpleEvent( %@ )",eventName);
+    [MSAnalytics trackEvent:eventName];
+}
+
+void GodotAppCenter::analyticsTrackComplexEvent(String name, String jsonProperties) {
+    NSString *eventName = [[[NSString alloc] initWithUTF8String:name.utf8().get_data()];
+
+    NSString *jsonString = [[NSString alloc] initWithData:data
+                                                 encoding:NSUTF8StringEncoding];
+    NSLog(@"AppCenter.Analytics: ComplexEvent( %@ : %@ )",eventName,jsonString);
+
+    NSError *error = nil;
+    NSDictionary *properties = [NSJSONSerialization JSONObjectWithData:data
+                                                         options:kNilOptions 
+                                                           error:&error];
+
+    if (!properties) {
+        NSLog(@"Error parsing JSON: %@", error);
+    } else {
+        [MSAnalytics trackEvent:eventName withProperties:properties];
+    }
 }
 
 void GodotAppCenter::_bind_methods() {
-    CLASS_DB::bind_method("start",&GodotAppCenter::start);
+    CLASS_DB::bind_method("isEnabled",&GodotAppCenter::isEnabled);
+    CLASS_DB::bind_method("setEnabled",&GodotAppCenter::setEnabled);
+    CLASS_DB::bind_method("setUserId",&GodotAppCenter::setUserId);
+    CLASS_DB::bind_method("setLogLevel",&GodotAppCenter::setLogLevel);
+    CLASS_DB::bind_method("analyticsTrackSimpleEvent",&GodotAppCenter::analyticsTrackSimpleEvent);
+    CLASS_DB::bind_method("analyticsTrackComplexEvent",&GodotAppCenter::analyticsTrackComplexEvent);
 }
